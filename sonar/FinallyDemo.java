6db3e11ced9787ee801fe8517fa93a4ce72b2251
nagaprudhvisai <228a1a4246@risekrishnasaiprakasam.edu.in>
2024-10-23 09:45:06 +0530

    Add files via upload

---
+++ sonar/FinallyDemo.java
@@ -0,0 +1,20 @@
+class FinallyDemo
+{
+public static void main(String args[])
+{
+try
+{
+int a=10,b=0;
+int c=a/b;
+System.out.println(c);
+}
+catch(ArithmeticException e)
+{
+System.out.println(e);
+}
+finally
+{
+System.out.println("this is inside finally block");
+}
+}
+}
